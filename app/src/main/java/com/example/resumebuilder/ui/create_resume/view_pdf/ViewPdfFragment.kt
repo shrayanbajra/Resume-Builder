fec5ded797b5abf40cc381d3f13cc9fb591d6eb7
Shrayan Bajracharya <shrayanbajracharya@gmail.com>
2022-04-06 23:17:47 +0545

    ViewPdfFragment -> Refactored code

--- app/src/main/java/com/example/resumebuilder/ui/create_resume/view_pdf/ViewPdfFragment.kt
+++ app/src/main/java/com/example/resumebuilder/ui/create_resume/view_pdf/ViewPdfFragment.kt
@@ -1,7 +1,6 @@
 package com.example.resumebuilder.ui.create_resume.view_pdf
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -17,7 +16,6 @@ import com.example.resumebuilder.ui.ResumeViewModel
 import com.example.resumebuilder.utils.Constants
 import com.itextpdf.text.*
 import com.itextpdf.text.pdf.BaseFont
-import com.itextpdf.text.pdf.PdfContentByte
 import com.itextpdf.text.pdf.PdfWriter
 import timber.log.Timber
 import java.io.*
@@ -30,11 +28,8 @@ class ViewPdfFragment : Fragment() {
 
     private var pdfFile: File? = null
     private var docWriter: PdfWriter? = null
-    var bfBold: BaseFont? = null
-    var bfNormal: BaseFont? = null
-    var bfItalic: BaseFont? = null
 
-    val headingFont = FontFactory.getFont(
+    private val headingFont: Font = FontFactory.getFont(
         "res/font/sf_pro_display_medium.OTF",
         BaseFont.IDENTITY_H,
         BaseFont.EMBEDDED,
@@ -70,20 +65,6 @@ class ViewPdfFragment : Fragment() {
     private fun openPdf() {
         val map = MimeTypeMap.getSingleton()
         val ext = MimeTypeMap.getFileExtensionFromUrl(pdfFile?.name)
-        var type = map.getMimeTypeFromExtension(ext)
-
-        if (type == null) type = "*/*"
-
-        val intent = Intent(Intent.ACTION_VIEW)
-        val data: Uri = Uri.fromFile(pdfFile)
-
-        intent.setDataAndType(data, type)
-        startActivity(intent)
-    }
-
-    private fun openPdfWithNewStyle() {
-        val map = MimeTypeMap.getSingleton()
-        val ext = MimeTypeMap.getFileExtensionFromUrl(pdfFile?.name)
         var mimeType = map.getMimeTypeFromExtension(ext)
 
         if (mimeType == null) mimeType = "*/*"
@@ -109,7 +90,7 @@ class ViewPdfFragment : Fragment() {
 
         try {
 
-            createPdfWrapper()
+            createResume()
 
         } catch (e: FileNotFoundException) {
 
@@ -125,54 +106,6 @@ class ViewPdfFragment : Fragment() {
     }
 
     @Throws(FileNotFoundException::class, DocumentException::class)
-    fun createPdfWrapper() {
-
-        createResume()
-
-    }
-
-    private fun initializeFonts() {
-        val font: Font = FontFactory.getFont(
-            "res/font/sf_pro_display_medium.OTF",
-            BaseFont.IDENTITY_H, BaseFont.EMBEDDED, 0.8f, Font.NORMAL, BaseColor.BLACK
-        )
-        bfNormal = font.getBaseFont()
-        val font1: Font = FontFactory.getFont(
-            "res/font/sf_pro_display_bold.OTF",
-            BaseFont.IDENTITY_H, BaseFont.EMBEDDED, 0.8f, Font.NORMAL, BaseColor.BLACK
-        )
-        bfBold = font1.getBaseFont()
-        val font2: Font = FontFactory.getFont(
-            "res/font/sf_pro_display_black_italic.OTF",
-            BaseFont.IDENTITY_H, BaseFont.EMBEDDED, 0.8f, Font.NORMAL, BaseColor.BLACK
-        )
-        bfItalic = font2.getBaseFont()
-    }
-
-    private fun createHeadings(
-        pdfContentByte: PdfContentByte,
-        x: Int,
-        y: Int,
-        text: String,
-        size: Int,
-        fontStyle: String,
-        color: BaseColor
-    ) {
-        pdfContentByte.beginText()
-
-        when (fontStyle) {
-            "normal" -> pdfContentByte.setFontAndSize(bfNormal, size.toFloat())
-            "bold" -> pdfContentByte.setFontAndSize(bfBold, size.toFloat())
-            "italic" -> pdfContentByte.setFontAndSize(bfItalic, size.toFloat())
-        }
-
-        pdfContentByte.setColorFill(color)
-        pdfContentByte.setTextMatrix(x.toFloat(), y.toFloat())
-        pdfContentByte.showText(text)
-        pdfContentByte.endText()
-    }
-
-    @Throws(DocumentException::class)
     private fun createResume() {
         val docsFolder = File(requireContext().getExternalFilesDir("Resumes"), "PDFs")
         if (!docsFolder.exists()) {
@@ -193,24 +126,6 @@ class ViewPdfFragment : Fragment() {
             e.printStackTrace()
         }
         document.open()
-        initializeFonts()
-        val cb = docWriter!!.directContent
-
-//        val rectangle1 = Rectangle(0F, 1000F, 800F, 600F)
-//        val myColor = BaseColor(42, 42, 42)
-//        rectangle1.backgroundColor = myColor
-//        cb.rectangle(rectangle1)
-
-        val black = BaseColor(0, 0, 0)
-        val white = BaseColor(255, 255, 255)
-        val font = FontFactory.getFont(
-            "res/font/sf_pro_display_medium.OTF",
-            BaseFont.IDENTITY_H,
-            BaseFont.EMBEDDED,
-            10f,
-            Font.NORMAL,
-            BaseColor.BLACK
-        )
 
         document.apply {
             addNameSection()
@@ -227,7 +142,7 @@ class ViewPdfFragment : Fragment() {
             output.close()
             docWriter!!.close()
 
-            openPdfWithNewStyle()
+            openPdf()
 
         } catch (e: IOException) {
             e.printStackTrace()
